67607da7411f93f0b024b3f73efa1852e5226904
Ciro <kalilinux1971@gmail.com>
2024-10-14 20:25:29 +0200

    start+

--- src/components/AstroFoto.vue
+++ src/components/AstroFoto.vue
@@ -1,12 +1,15 @@
 <template>
   <q-card class="my-card">
-    <img src="https://cdn.quasar.dev/img/mountains.jpg">
     <q-card-section>
-      <div class="text-h6">Our Changing Planet</div>
-      <div class="text-subtitle2">by John Doe</div>
+      <div class="text-h5">campionamento di un’immagine</div>
+      <div class="text-subtitle2">C = (Dp /F) x 206265</div>
+      <div class="text-subtitle1">C = campionamento (in secondi d’arco su pixel)</div>
+      <div class="text-subtitle1">Dp = dimensioni dei pixel del sensore utilizzato</div>
+      <div class="text-subtitle1">F = focale del telescopio</div>
+      <div class="text-subtitle1">206265 è il fattore di conversione tra radianti e secondi d’arco</div>
     </q-card-section>
     <q-card-section>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      campionamento di un’immagine
     </q-card-section>
   </q-card>
 </template>
@@ -18,6 +21,6 @@
 <style lang="scss" scoped>
 .my-card{
   width: 100%;
-  max-width: 250px;
+  max-width: 450px;
 }
 </style>
