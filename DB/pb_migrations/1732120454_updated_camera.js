fe56ff177c2fa11ac9ebe2a29b660a1bba50146e
Ciro <kalilinux1971@gmail.com>
2024-11-20 19:37:12 +0100

    dbUpdate

---
+++ DB/pb_migrations/1732120454_updated_camera.js
@@ -0,0 +1,31 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sx7qf0u94aoqzq0")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "qnvvhhnw",
+    "name": "noise",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sx7qf0u94aoqzq0")
+
+  // remove
+  collection.schema.removeField("qnvvhhnw")
+
+  return dao.saveCollection(collection)
+})
